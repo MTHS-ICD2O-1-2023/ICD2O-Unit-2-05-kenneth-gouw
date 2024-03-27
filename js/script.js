66e106dc2f553201be29bf6dc42a8aae6f5b15d3
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-03-27 17:23:14 +0000

    ffgggggg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Kenneth
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+
+function findIncomeClicked() {
+  const hoursInput = parseInt(document.getElementById("hours-entered").value)
+  const incomeInput = parseInt(document.getElementById("income-entered").value)
+  const govTax = 0.18 * (hoursInput * incomeInput)
+  const totalEarnings = (hoursInput * incomeInput) - govTax
+  document.getElementById("answer").innerHTML = "Your total income is: $" + totalEarnings.toFixed(2) + "<br>" + "The government will take: $" + govTax.toFixed(2)
+}
